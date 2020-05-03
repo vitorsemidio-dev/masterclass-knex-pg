713c88b9eedb846033df282f9800cefde0028cc3
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-05-03 19:04:38 -0300

    feat: rota não encontrada

--- src/server.js
+++ src/server.js
@@ -7,6 +7,12 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
+app.use((req, rex, next) => {
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+});
+
 app.use((error, req, res, next) => {
   res.status(error.status || 500);
   res.json({ error: error.message });
