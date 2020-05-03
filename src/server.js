e0ab6679f6c871a163eda1ba840a0b19367c8003
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-05-03 18:24:25 -0300

    feat: rota users

--- src/server.js
+++ src/server.js
@@ -1,6 +1,14 @@
 const express = require('express');
 
+const knex = require('./database');
 const app = express();
+
+app.get('/users', (req, res) => {
+  knex('users').then((results) => {
+    return res.json(results);
+  });
+});
+
 
 app.listen(3333, () => {
   console.log('Server is running');
